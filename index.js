11f41b708e7745a0a8b5b63ee4f43688af252703
Sudhanshu Singh <sudhanshu3430@gmail.com>
2024-10-28 12:08:15 +0530

    first

--- index.js
+++ index.js
@@ -1,84 +1,57 @@
 require('dotenv').config();
 const { google } = require('googleapis');
+const nodemailer = require('nodemailer'); // Using nodemailer
 const path = require('path');
-const fs = require('fs');
 
-// const CLIENT_ID = process.env.CLIENT_ID;
-// const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// const REDIRECT_URI = process.env.REDIRECT_URI;
-// const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-const SERVICE_AUTH_PATH = process.env.SERVICE_AUTH_PATH;
+// Load your OAuth2 credentials
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
-const SERVICE_ACCOUNT_FILE = path.join(__dirname, SERVICE_AUTH_PATH);
+// Create an OAuth2 client
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
+// Set refresh token
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-// const oauth2Client = new google.auth.OAuth2(
-//     CLIENT_ID,
-//     CLIENT_SECRET,
-//     REDIRECT_URI
-// )
-
-const auth = new google.auth.GoogleAuth({
-    keyFile: SERVICE_ACCOUNT_FILE,
-    scopes: ['https://www.googleapis.com/auth/drive']
-});
-
-// oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-const drive = google.drive({
-    version: 'v3',
-    auth: auth
-});
-
-const filePath = path.join(__dirname, 'list-users.png');
-
-async function uploadFile() {
-
+// Function to create and send the email
+const sendMail = async (options) => {
     try {
-        const response = await drive.files.create({
-            requestBody: {
-                name: 'sample-new.png',
-                mimeType: 'image/png'
+        const accessToken = await oAuth2Client.getAccessToken();
+
+        // Create transporter with OAuth2
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'sudhanshu3430@gmail.com', // Email of the user you want to impersonate
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken.token,
             },
-            media: {
-                mimeType: 'image/png',
-                body: fs.createReadStream(filePath)
-            }
         });
 
-        console.log(response.data);
-        console.log('File ID:', response.data.id);
-        
+        // Send mail
+        const info = await transporter.sendMail(options);
+        console.log('Email sent successfully:', info.messageId);
     } catch (error) {
-        console.log(error.message)
-        
+        console.error('Error sending email:', error);
     }
-    
-}
+};
 
-async function downloadFile(fileId, dest) {
-    const destPath = path.join(__dirname, dest);
+// Email options
+const options = {
+    from: 'sudhanshu3430@gmail.com',
+    to: 'infiniteloop558@gmail.com',
+    subject: 'Hello World 🚀',
+    text: 'This email is sent from the command line',
+    headers: [
+        { key: 'X-Application-Developer', value: 'Sudhanshu Singh' },
+        { key: 'X-Application-Version', value: 'v1.0.0.2' },
+    ],
+};
 
-    const fileStream = fs.createWriteStream(destPath);
-
-    try {
-        const response = await drive.files.get({
-            fileId: fileId,
-            alt: 'media'
-        }, { responseType: 'stream' });
-
-        response.data
-            .on('end', () => {
-                console.log('File downloaded successfully to:', destPath);
-            })
-            .on('error', (err) => {
-                console.error('Error downloading file:', err);
-            })
-            .pipe(fileStream);
-    } catch (error) {
-        console.error('Error fetching file:', error.message);
-    }
-}
-
-// uploadFile();
-downloadFile('1pGShD90SrAnFNYKI4C5nKo6xHRUs_Vt7', 'downloaded_file.png');
+// Call the sendMail function
+sendMail(options);
